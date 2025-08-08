353eb74cb8e8fbc302587400d6fae9215fce56a2
Béla Zoltán Szabó <szabobelazoltan@gmail.com>
2025-08-08 20:12:23 +0200

    Project base

---
+++ src/main/java/hu/szbz/hbc/doals/DoalsApplication.java
@@ -0,0 +1,13 @@
+package hu.szbz.hbc.doals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoalsApplication.class, args);
+	}
+
+}
